2145ca13a7f10187d71f18f152c33f9a45e39cbb
Stephen Meier <info@stephenmeier.net>
2018-01-29 12:52:29 -0500

    adding assimp source to build from source

---
+++ contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/srmeier/Desktop/assimp-3.3.1/contrib/zlib
+# Build directory: C:/Users/srmeier/Desktop/assimp-3.3.1/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
